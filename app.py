dcac2898488cf664b3627954cf1c45c126f84878
vanthana-tech <vanthanakannan07@gmail.com>
2026-09-07 20:37:00 +0530

    Change import from tflite_runtime to tensorflow

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
-import tflite_runtime.interpreter as tflite
+import tensorflow as tflite
 import json
 import os
 from remedies import get_remedy
